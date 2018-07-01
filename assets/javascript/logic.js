719f6e520ef8ddd64aa5088851188a28967bf39d
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-07-01 18:59:24 -0500

    added login modal

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,11 @@
 $(document).ready(function() { // DOCUMENT READY OPENING
 
+
+document.getElementById("login").addEventListener("click", loginUser);
+
+function loginUser() {
+    console.log("it works!")
+    
+}
+
 }); // DOCUMENT READY CLOSING
