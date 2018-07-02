40c404c7d456821de4f8b9e885370ae40d2d1d8e
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-07-01 19:34:30 -0500

    added password protection and username input

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,10 +1,26 @@
 $(document).ready(function() { // DOCUMENT READY OPENING
 
+  // Initialize Firebase
+  let config = {
+    apiKey: "AIzaSyCvr9aO-x8SQyf2E5_go8RDgYFOGv_Bx_g",
+    authDomain: "immirex-b56dd.firebaseapp.com",
+    databaseURL: "https://immirex-b56dd.firebaseio.com",
+    projectId: "immirex-b56dd",
+    storageBucket: "",
+    messagingSenderId: "533658082982"
+  };
+  firebase.initializeApp(config);
 
-document.getElementById("login").addEventListener("click", loginUser);
+
+document.getElementById("login-submit").addEventListener("click", loginUser);
 
 function loginUser() {
-    console.log("it works!")
+    let username = document.getElementById("inputUserName").value;
+    let password = document.getElementById("inputPassword").value;
+    console.log(username);
+    console.log(password);
+
+
     
 }
 
